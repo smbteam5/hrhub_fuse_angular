08d35afcd11c4953b313d93e04d5cc883c7bd3c3
Vishnu-nas <vishnupp@newagesmb.com>
2023-02-08 19:10:36 +0530

    new

--- src/app/onboarding/onboarding.component.ts
+++ src/app/onboarding/onboarding.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+export interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-onboarding',
   templateUrl: './onboarding.component.html',
   styleUrls: ['./onboarding.component.scss']
 })
+
 export class OnboardingComponent implements OnInit {
 
-  constructor() { }
+  selectedFood: string;
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.selectedFood = this.foods[1].value;
   }
 
 }
